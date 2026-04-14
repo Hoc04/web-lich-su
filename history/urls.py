423871e5a6f9412d00c820465faba3893586895a
Hoc04 <emailtrinhthanhhoc12@gmail.com>
2026-04-14 18:40:44 +0700

    Complete project: add question.html, video.html, update urls

--- history/urls.py
+++ history/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('video/', views.video_page, name='video'),
-    path('question/', views.question_page, name='question'),
+    path('video/', views.video_page, name='video_page'),
+    path('question/', views.question_page, name='question_page'),
 ]
